93c5c94b0f9b077cdb8a25a6fd5ab9b28e65ecef
Alexander Bulychev <asbul@yandex.ru>
2022-06-30 16:42:55 +0300

    2 / Make autoresizing two subviews in code

--- AutoLayout_Examples/TileView.swift
+++ AutoLayout_Examples/TileView.swift
@@ -7,7 +7,15 @@
 
 import UIKit
 
+@IBDesignable
 class TileView: UIView {
+
+    @IBInspectable var padding: CGFloat = 25.0 {
+        didSet {
+            setNeedsLayout()
+        }
+    }
+
     override init(frame: CGRect) {
         super.init(frame: frame)
         setupView()
@@ -19,11 +27,33 @@ class TileView: UIView {
     }
 
     private func setupView() {
-        // setup view code goes here
+        addSubview(blueView)
+        addSubview(redView)
     }
 
     override func layoutSubviews() {
         super.layoutSubviews()
-        // adjust subviews here
+        let containerWidth = bounds.width
+        let containerHeight = bounds.height
+
+        let numberOfItems: CGFloat = 2
+        let itemWidth = (containerWidth - (numberOfItems + 1) * padding) / numberOfItems
+        let itemHeight = containerHeight - 2 * padding
+
+        blueView.frame = CGRect(x: padding, y: padding, width: itemWidth, height: itemHeight)
+        redView.frame = CGRect(x: 2 * padding + itemWidth, y: padding, width: itemWidth, height: itemHeight)
     }
 }
+
+private let redView: UIView = {
+    let view = UIView()
+    view.backgroundColor = .red
+    return view
+}()
+
+private let blueView: UIView = {
+    let view = UIView()
+    view.backgroundColor = .blue
+    return view
+}()
+
